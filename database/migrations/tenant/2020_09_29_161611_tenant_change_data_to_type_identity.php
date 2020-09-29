2efea33d4cd6a12e3b1c11cbc6d6a17789da35fe
Cristian Gonzalez <cristian.ballon@gmail.com>
2020-09-29 17:02:24 -0500

    fix | cambio data identoty

--- database/migrations/tenant/2020_09_29_161611_tenant_change_data_to_type_identity.php
+++ database/migrations/tenant/2020_09_29_161611_tenant_change_data_to_type_identity.php
@@ -15,6 +15,20 @@ class TenantChangeDataToTypeIdentity extends Migration
     {
         if (Schema::hasTable('co_type_identity_documents')) {
 
+            DB::table('co_type_identity_documents')->where('id', 1)->update(['name' => 'Registro civil', 'code' => '11a']);
+            DB::table('co_type_identity_documents')->where('id', 2)->update(['name' => 'Tarjeta de identidad', 'code' => '12a']);
+            DB::table('co_type_identity_documents')->where('id', 3)->update(['name' => 'Cédula de ciudadanía', 'code' => '13a']);
+            DB::table('co_type_identity_documents')->where('id', 4)->update(['name' => 'Tarjeta de extranjería', 'code' => '21a']);
+            DB::table('co_type_identity_documents')->where('id', 5)->update(['name' => 'Cédula de extranjería', 'code' => '22a']);
+
+            DB::table('co_type_identity_documents')->where('id', 6)->update(['name' => 'NIT', 'code' => '31a']);
+            DB::table('co_type_identity_documents')->where('id', 7)->update(['name' => 'Pasaporte', 'code' => '41a']);
+            DB::table('co_type_identity_documents')->where('id', 8)->update(['name' => 'Documento de identificación extranjero', 'code' => '42a']);
+            DB::table('co_type_identity_documents')->where('id', 9)->update(['name' => 'NIT de otro país', 'code' => '50a']);
+            DB::table('co_type_identity_documents')->where('id', 10)->update(['name' => 'NUIP *', 'code' => '91a']);
+
+
+
             DB::table('co_type_identity_documents')->where('id', 1)->update(['name' => 'Registro civil', 'code' => '11']);
             DB::table('co_type_identity_documents')->where('id', 2)->update(['name' => 'Tarjeta de identidad', 'code' => '12']);
             DB::table('co_type_identity_documents')->where('id', 3)->update(['name' => 'Cédula de ciudadanía', 'code' => '13']);
