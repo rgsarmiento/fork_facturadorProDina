55b036be1c0e2077e9d5196adb51d2a2d9351310
Cristian Gonzalez <cristian.ballon@gmail.com>
2020-09-29 16:37:24 -0500

    fix | cambios en data co_type_identity_documents

--- database/migrations/tenant/2020_09_29_161611_tenant_change_data_to_type_identity.php
+++ database/migrations/tenant/2020_09_29_161611_tenant_change_data_to_type_identity.php
@@ -13,17 +13,23 @@ class TenantChangeDataToTypeIdentity extends Migration
      */
     public function up()
     {
-        DB::table('co_type_identity_documents')->where('id', 1)->update(['name' => 'Registro civil', 'code' => '11']);
-        DB::table('co_type_identity_documents')->where('id', 2)->update(['name' => 'Tarjeta de identidad', 'code' => '12']);
-        DB::table('co_type_identity_documents')->where('id', 3)->update(['name' => 'Cédula de ciudadanía', 'code' => '13']);
-        DB::table('co_type_identity_documents')->where('id', 4)->update(['name' => 'Tarjeta de extranjería', 'code' => '21']);
-        DB::table('co_type_identity_documents')->where('id', 5)->update(['name' => 'Cédula de extranjería', 'code' => '22']);
+        if (Schema::hasTable('co_type_identity_documents')) {
 
-        DB::table('co_type_identity_documents')->where('id', 6)->update(['name' => 'NIT', 'code' => '31']);
-        DB::table('co_type_identity_documents')->where('id', 7)->update(['name' => 'Pasaporte', 'code' => '41']);
-        DB::table('co_type_identity_documents')->where('id', 8)->update(['name' => 'Documento de identificación extranjero', 'code' => '42']);
-        DB::table('co_type_identity_documents')->where('id', 9)->update(['name' => 'NIT de otro país', 'code' => '50']);
-        DB::table('co_type_identity_documents')->where('id', 10)->update(['name' => 'NUIP *', 'code' => '91']);
+            DB::table('co_type_identity_documents')->where('id', 1)->update(['name' => 'Registro civil', 'code' => '11']);
+            DB::table('co_type_identity_documents')->where('id', 2)->update(['name' => 'Tarjeta de identidad', 'code' => '12']);
+            DB::table('co_type_identity_documents')->where('id', 3)->update(['name' => 'Cédula de ciudadanía', 'code' => '13']);
+            DB::table('co_type_identity_documents')->where('id', 4)->update(['name' => 'Tarjeta de extranjería', 'code' => '21']);
+            DB::table('co_type_identity_documents')->where('id', 5)->update(['name' => 'Cédula de extranjería', 'code' => '22']);
+
+            DB::table('co_type_identity_documents')->where('id', 6)->update(['name' => 'NIT', 'code' => '31']);
+            DB::table('co_type_identity_documents')->where('id', 7)->update(['name' => 'Pasaporte', 'code' => '41']);
+            DB::table('co_type_identity_documents')->where('id', 8)->update(['name' => 'Documento de identificación extranjero', 'code' => '42']);
+            DB::table('co_type_identity_documents')->where('id', 9)->update(['name' => 'NIT de otro país', 'code' => '50']);
+            DB::table('co_type_identity_documents')->where('id', 10)->update(['name' => 'NUIP *', 'code' => '91']);
+
+        }
+
+
 
     }
 
@@ -34,6 +40,6 @@ class TenantChangeDataToTypeIdentity extends Migration
      */
     public function down()
     {
-        
+        Schema::dropIfExists('co_type_identity_documents');
     }
 }
