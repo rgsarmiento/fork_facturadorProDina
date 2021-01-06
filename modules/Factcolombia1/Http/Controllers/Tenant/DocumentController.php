556c2d4b10b74d12edd32a4452d06de57e0480e7
Alexander Obando <gerencia@torresoftware.com>
2021-01-06 15:23:47 -0500

    Envio municipality_id_fact

--- modules/Factcolombia1/Http/Controllers/Tenant/DocumentController.php
+++ modules/Factcolombia1/Http/Controllers/Tenant/DocumentController.php
@@ -842,7 +842,7 @@ class DocumentController extends Controller
     public function table($table)
     {
         if ($table === 'customers') {
-            $customers = Person::whereType('customers')->whereIsEnabled()->orderBy('name')->take(20)->get()->transform(function($row) {
+            $customers = Person::whereType('customers')->whereIsEnabled()->orderBy('name')->get()->transform(function($row) {
                 return [
                     'id' => $row->id,
                     'description' => $row->number.' - '.$row->name,
