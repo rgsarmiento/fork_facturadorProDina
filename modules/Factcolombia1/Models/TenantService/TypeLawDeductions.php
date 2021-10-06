a08f100968412427a0ebb902381fdd40e91ed53a
Alexander Obando <gerencia@torresoftware.com>
2021-10-06 17:39:28 -0500

    Migraciones y modelos para nomina electronica.

---
+++ modules/Factcolombia1/Models/TenantService/TypeLawDeductions.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Modules\Factcolombia1\Models\TenantService;
+
+use Illuminate\Database\Eloquent\Model;
+use Hyn\Tenancy\Traits\UsesTenantConnection;
+
+class TypeLawDeductions extends Model
+{
+    use  UsesTenantConnection;
+
+    protected $table = 'co_type_law_deductions';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'code',
+    ];
+}
