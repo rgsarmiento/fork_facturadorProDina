ffb101cf771c68383c21c8b52e60cedec43cdee9
Alexander Obando <gerencia@torresoftware.com>
2022-04-28 14:31:53 -0500

    Problema al enviar peticion al crear compañia cuando la api tiene ssl y el facturador no

--- modules/Factcolombia1/Helpers/HttpConnectionApi.php
+++ modules/Factcolombia1/Helpers/HttpConnectionApi.php
@@ -1,13 +1,13 @@
 <?php
 
 namespace Modules\Factcolombia1\Helpers;
- 
+
 use Illuminate\Support\Facades\Log;
 use Exception;
 
 class HttpConnectionApi
 {
- 
+
     protected $api_token;
     protected $base_url;
 
@@ -23,26 +23,28 @@ class HttpConnectionApi
         try {
 
             $ch = curl_init("{$this->base_url}{$url}");
-            
+
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
             curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
             curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($params));
+            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
             curl_setopt($ch, CURLOPT_HTTPHEADER, array(
                 'Content-Type: application/json',
                 'Accept: application/json',
                 "Authorization: Bearer {$this->api_token}"
             ));
-    
+
             $response = curl_exec($ch);
             $curl_error = curl_error($ch);
-    
+
             if($curl_error) return $this->responseMessage(false, 'Error en la petición a la Api');
 
             // dd($response);
-    
+
             return json_decode($response, true);
 
-        }catch (Exception $e) 
+        }catch (Exception $e)
         {
             return $this->responseError($e);
         }
@@ -74,33 +76,34 @@ class HttpConnectionApi
         try {
 
             $ch = curl_init("{$this->base_url}{$url}");
-            
+
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
             curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
-
+            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
             curl_setopt($ch, CURLOPT_HTTPHEADER, array(
                 'Content-Type: application/json',
                 'Accept: application/json',
                 "Authorization: Bearer {$this->api_token}"
             ));
-    
+
             $response = curl_exec($ch);
             $curl_error = curl_error($ch);
-    
+
             if($curl_error) return $this->responseMessage(false, 'Error en la petición a la Api');
 
             return json_decode($response, true);
 
-        }catch (Exception $e) 
+        }catch (Exception $e)
         {
             return $this->responseError($e);
         }
 
     }
-    
+
     public function parseErrorsToString($errors)
     {
-        
+
         $message = "Error de validación Api: ";
 
         foreach ($errors as $key => $value) {
