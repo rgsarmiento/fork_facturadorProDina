eb7193171a46f06db0fa7cf322e1d44e35fe9d9f
Alexander Obando <gerencia@torresoftware.com>
2023-03-09 15:30:39 -0500

    Error al recibir facturas sin mailfromname

--- modules/RadianEvent/Http/Controllers/SearchEmailController.php
+++ modules/RadianEvent/Http/Controllers/SearchEmailController.php
@@ -23,18 +23,18 @@ use Modules\RadianEvent\Http\Resources\{
 };
 use Modules\RadianEvent\Helpers\ZipHelper;
 use Illuminate\Support\Facades\DB;
-use Modules\Payroll\Traits\UtilityTrait; 
+use Modules\Payroll\Traits\UtilityTrait;
 use Carbon\Carbon;
 
 
 class SearchEmailController extends Controller
 {
-    
+
     use UtilityTrait;
 
-        
+
     /**
-     * 
+     *
      * Eventos Radian
      * Buscar correos del servidor imap, validarlos, cargar archivos, registrar en documentos recibidos
      *
@@ -44,7 +44,7 @@ class SearchEmailController extends Controller
     public function searchImapEmails(Request $request)
     {
         $init_time = Carbon::now();
-        
+
         $emails = $this->getEmails($request);
 
         if(!$emails['success']) return $emails;
@@ -58,60 +58,60 @@ class SearchEmailController extends Controller
             'search_start_date' => $request->search_start_date,
             'search_end_date' => $request->search_end_date,
         ]);
-        
-        try 
+
+        try
         {
             $data = DB::connection('tenant')->transaction(function() use($emails, $email_reading, $init_time) {
-            
+
                 $all_emails_id = $emails['emails'];
                 $mailbox = $emails['mailbox'];
-                
+
                 $selected_emails = [];
-        
-                foreach ($all_emails_id as $key => $email_id) 
+
+                foreach ($all_emails_id as $key => $email_id)
                 {
                     $mail = $mailbox->getMail($email_id);
-        
+
                     // validar si el correo cumple las condiciones
                     $data_upload = null;
-        
+
                     if($this->isValidEmail($mail, $email_reading))
                     {
                         // obtener archivos del correo
                         $attachment = collect($mail->getAttachments())->first();
                         $attachment_content = $attachment->getContents(); //zip
-        
+
                         $filename = $attachment->name;
-        
+
                         $extract_zip = (new ZipHelper())->extractZip($attachment_content);
-        
+
                         if(count($extract_zip) === 2) // se valida si tiene 2 archivos, xml y pdf
                         {
                             $xml_filename = $extract_zip[0]['filename'];
                             $xml_content = $extract_zip[0]['content'];
-            
+
                             $pdf_filename = $extract_zip[1]['filename'];
                             $pdf_content = $extract_zip[1]['content'];
-        
+
                             if(str_contains($xml_filename, '.xml') && str_contains($pdf_filename, '.pdf'))
                             {
                                 // verificar si existe el xml
                                 $exist_received_document = ReceivedDocument::select('id')->where('xml', $xml_filename)->first();
-        
+
                                 if(!$exist_received_document)
                                 {
                                     // enviar api para parsear xml y obtener data
                                     $send_request_to_api = $this->sendXmlToApi($xml_content);
-        
+
                                     // registrar en bd
                                     $email_reading_detail = $this->saveEmailReadingDetail($email_reading, $mail);
-                                    
+
                                     if($send_request_to_api['success'])
                                     {
                                         $data = $send_request_to_api['data'];
                                         $data['xml'] = $xml_filename;
                                         $data['pdf'] = $pdf_filename;
-                                        
+
                                         $this->updateEmailReadingDetail($email_reading_detail, [
                                             'success' => true,
                                             'response_api' => $data
@@ -120,7 +120,7 @@ class SearchEmailController extends Controller
                                         //insertar detalle
                                         $email_reading_detail->received_document()->create($data);
 
-                                        //subir archivo 
+                                        //subir archivo
                                         $this->uploadFile($pdf_filename, $pdf_content);
                                         $this->uploadFile($xml_filename, $xml_content);
 
@@ -149,7 +149,7 @@ class SearchEmailController extends Controller
 
                 $end_time = Carbon::now();
                 $diff_in_seconds = $end_time->diffInSeconds($init_time);
-                
+
                 return [
                     'success' => true,
                     'message' => "Proceso realizado correctamente: {$email_reading->details()->count()} correos fueron registrados.",
@@ -161,8 +161,8 @@ class SearchEmailController extends Controller
             });
 
             return $data;
-        } 
-        catch (Exception $e) 
+        }
+        catch (Exception $e)
         {
             $email_reading->errors = $e->getMessage();
             return $this->getErrorFromException($e->getMessage(), $e);
@@ -170,24 +170,24 @@ class SearchEmailController extends Controller
 
     }
 
-    
+
     /**
-     * 
+     *
      * Conectarse con servidor imap y obtener correos, aplicar criterios de busqueda
      *
      * @param  Request $request
      * @return array
      */
     private function getEmails(Request $request)
-    { 
+    {
 
         $advanced_configuration = AdvancedConfiguration::selectImapColumns()->firstOrFail();
 
         $imap_server = '{'.$advanced_configuration->radian_imap_host.':'.$advanced_configuration->radian_imap_port.'/imap/'.$advanced_configuration->radian_imap_encryption.'}INBOX';
-        
+
         $mailbox = new \PhpImap\Mailbox($imap_server, $advanced_configuration->radian_imap_user, $advanced_configuration->radian_imap_password);
-        
-        try 
+
+        try
         {
             $criteria = $this->getSearchCriteria($request);
 
@@ -206,16 +206,16 @@ class SearchEmailController extends Controller
             ];
 
         }
-        catch(PhpImap\Exceptions\ConnectionException $ex) 
+        catch(PhpImap\Exceptions\ConnectionException $ex)
         {
             return $this->getGeneralResponse(false, 'Conexión IMAP fallida: ' . implode(",", $ex->getErrors('all')));
         }
 
     }
 
-    
+
     /**
-     * 
+     *
      * Aplicar criterios de busqueda por rangos de fecha
      *
      * @param  Request $request
@@ -245,9 +245,9 @@ class SearchEmailController extends Controller
         return $criteria;
     }
 
-        
+
     /**
-     * 
+     *
      * Actualizar detalle con respuesta de validaciones de la Api
      *
      * @param  EmailReadingDetail $email_reading_detail
@@ -260,9 +260,9 @@ class SearchEmailController extends Controller
         $email_reading_detail->save();
     }
 
-    
+
     /**
-     * 
+     *
      * Registrar detalle del email
      *
      * @param  EmailReading $email_reading
@@ -271,18 +271,19 @@ class SearchEmailController extends Controller
      */
     private function saveEmailReadingDetail($email_reading, $mail)
     {
+
         return $email_reading->details()->create([
             'email_user' => $email_reading->email_user,
             'email_id' => $mail->id,
             'subject' => $mail->subject,
             'from_host' => $mail->fromHost,
-            'from_name' => $mail->fromName,
+            'from_name' => $mail->fromName ? $mail->fromName : $mail->fromAddress,
             'from_address' => $mail->fromAddress,
             'sender_host' => $mail->senderHost,
         ]);
     }
 
-    
+
     /**
      *
      * @param  string $filename
@@ -295,10 +296,10 @@ class SearchEmailController extends Controller
         Storage::disk('tenant')->put($folder.DIRECTORY_SEPARATOR.$filename, $content);
     }
 
-    
+
     /**
-     * 
-     * Enviar xml a la api para validar datos 
+     *
+     * Enviar xml a la api para validar datos
      * Obtener datos parseados para el registro en documentos recibidos
      *
      * @param  string $xml_content
@@ -308,7 +309,7 @@ class SearchEmailController extends Controller
     {
         $company = ServiceCompany::select('identification_number', 'api_token')->firstOrFail();
         $connection_api = new HttpConnectionApi($company->api_token);
-        
+
         $params = [
             'xml_document' => base64_encode($xml_content),
             'company_idnumber' => $company->identification_number,
@@ -317,9 +318,9 @@ class SearchEmailController extends Controller
         return $connection_api->sendRequestToApi('process-seller-document-reception', $params, 'POST');
     }
 
-    
+
     /**
-     * 
+     *
      * Validar si email cumple con las condiciones para procesarlo
      *
      * @param  $mail
@@ -344,7 +345,7 @@ class SearchEmailController extends Controller
             {
                 $type_document_code = trim($parse_subject[3]);
                 $exist_type_document = TypeDocument::where('code', $type_document_code)->select('id')->first();
-    
+
                 if($quantity_items === 5 && is_numeric($parse_subject[0]) && $exist_type_document && $mail->hasAttachments())
                 {
                     if(count($mail->getAttachments()) === 1)
